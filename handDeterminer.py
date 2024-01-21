e4c4f2253f8425b3a9054c9aae60ec8c50bebac0
Remi Arnaud <remi@acm.org>
2024-01-21 01:21:10 -0800

    using fast bit operators

--- handDeterminer.py
+++ handDeterminer.py
@@ -17,28 +17,111 @@ rankDict = {0:'high card',1:'pair',2:'two pair',3:'three of a kind',4:'straight'
 			6:'full house',7:'four of a kind',8:'straight flush',9:'royal flush'}
 
 def findWinner(players, table):
-	ranks = [] # having annoying array/list/ndarray datatype issues
+	ranks = []
 	bestHands = []
-	for i in range(len(players)):
-		possHands = list(combinations(players[i].hand+table,5))
+	bestHands_ = []
+	for player in players:
+		possHands = list(combinations(player.hand+table,5))
 		possHands = np.array(possHands)
-		print('player',players[i].playerNum)
-		rank,bestHand = determineBestHand(possHands)
-		ranks.append(rank)
-		bestHands.append(bestHand)
-	bestRank = np.max(ranks)
-	winnerIdx = np.where(ranks==bestRank)[0][:]
-	if len(winnerIdx) == 1:
-		print("Player",winnerIdx[0]+1,"wins with",rankDict[bestRank])
+		print()
+		print('player',player.playerNum)
+		print()
+		bestHands.append(determineBestHand(possHands))
+
+		bestHands_.append(determineBestHand_(player.hand+table))
+
+lookup_bits = {'2': 1, '3': 2, '4': 4, '5': 8, '6': 16, '7': 32, '8': 64, '9': 128, '10': 256, '11': 512, '12': 1024, '13': 2048, '14': 4096}
+
+def ffs(x):
+    """Returns the index, counting from 0, of the
+    least significant set bit in `x`.
+    """
+    return (x&-x).bit_length()-1
+
+def clz(x):
+	"""Returns the number of leading zero bits in `x`.
+	"""
+	return x.bit_length() - 1
+
+def maxConsecutiveOnes(x):
+	"""Returns the largest count of consecutive 1s in `x`.
+	   TODO: use ffs/clz to make this faster.
+	"""
+	count = 0
+	while (x!=0):
+		# This operation reduces length
+        # of every sequence of 1s by one.
+		x = (x & (x << 1))
+		count=count+1
+	return count
+
+def determineBestHand_(hand):
+	#convert hand in a bitfield
+	diamonds = 0
+	hearts = 0
+	spades = 0
+	clubs = 0
+	for card in hand:
+		match card[0]:
+			case 'd':
+				diamonds |= lookup_bits[card[1:]]
+			case 'h':
+				hearts |= lookup_bits[card[1:]]
+			case 's':
+				spades |= lookup_bits[card[1:]]
+			case 'c':
+				clubs |= lookup_bits[card[1:]]
+	print(hand)
+	#check for pairs
+	pairs = (diamonds & hearts).bit_count() + (diamonds & spades).bit_count() + (diamonds & clubs).bit_count() + (hearts & spades).bit_count() + (hearts & clubs).bit_count() + (spades & clubs).bit_count()
+	print ('pair=',pairs)
+	#check for three of a kind
+	three_of_a_kinds = (diamonds & hearts & spades).bit_count() + (diamonds & hearts & clubs).bit_count() + (diamonds & spades & clubs).bit_count() + (hearts & spades & clubs).bit_count()
+	print ('three of a kind=',three_of_a_kinds)
+	#check for four of a kind
+	four_of_a_kinds = (diamonds & hearts & spades & clubs).bit_count()
+	print ('four of a kind=',four_of_a_kinds)
+	#check for flush
+	flushes = (diamonds.bit_count() >= 5) + (hearts.bit_count() >= 5) + (spades.bit_count() >= 5) + (clubs.bit_count() >= 5)
+	print ('flush=',flushes)
+	#check for straight
+	straights = max(0,maxConsecutiveOnes(diamonds&hearts&spades&clubs)-4)
+	print ('straight=',straights)
+	#check for straight flush
+	straight_flushes = max(0,maxConsecutiveOnes(diamonds)-4)+max(0,maxConsecutiveOnes(hearts)-4)+max(0,maxConsecutiveOnes(spades)-4)+max(0,maxConsecutiveOnes(clubs)-4)
+	print ('straight flush=',straight_flushes)
+	#check for royal flush
+	royal_flush_test = (diamonds & 0x1E0) == 0x1E0 or (hearts & 0x1E0) == 0x1E0 or (spades & 0x1E0) == 0x1E0 or (clubs & 0x1E0) == 0x1E0
+	print ('royal flush test=',royal_flush_test)
+	#check for full house (3+2)
+	full_house_test = (pairs >= 2) and (three_of_a_kinds >= 1)
+	print ('full house test=',full_house_test)
+	mask = ~((diamonds & hearts & spades) | (diamonds & hearts & clubs) | (diamonds & spades & clubs) | (hearts & spades & clubs))
+	exact_pairs = (mask & diamonds & hearts).bit_count() + (mask & diamonds & spades).bit_count() + (mask & diamonds & clubs).bit_count() + (mask & hearts & spades).bit_count() + (mask & hearts & clubs).bit_count() + (mask & spades & clubs).bit_count()
+	print ('exact pairs=',exact_pairs)
+	print ('mask=',bin(mask& 0xFFFFFFFF)[-16:])
+
+	if (royal_flush_test):
+		return ROYAL_FLUSH
+	elif (straight_flushes != 0):
+		return STRAIGHT_FLUSH
+	elif (four_of_a_kinds !=0):
+		return FOUR_OF_A_KIND
+	elif (full_house_test):
+		return FULL_HOUSE
+	elif (flushes != 0):
+		return FLUSH
+	elif (straights != 0):
+		return STRAIGHT
+	elif (three_of_a_kinds != 0):
+		return THREE_OF_A_KIND
+	elif (pairs >= 2):
+		return TWO_PAIR
+	elif (pairs != 0):
+		return PAIR
 	else:
-		for i in range(len(players)):
-			'''
-			need to implement tie breaking/pot splitting
-
-			pot split when players have the exact same rank and/or card values
-			tie breaking when players have same rank
-			'''
-
+		return HIGH_CARD
+	
 def determineBestHand(possHands):
 	# Find the best 5 card hand for each player
 	ranks = np.zeros(len(possHands))
@@ -46,19 +129,23 @@ def determineBestHand(possHands):
 	for i in range(len(possHands)):
 		ranks[i] = determineHand(possHands[i])
 	bestRank = np.max(ranks)
-	print('best rank : ',rankDict[bestRank])
+	print('ranks',ranks)
 	bestHandIdx = np.where(ranks==bestRank)[0][:]
+	print('best hand indeces',bestHandIdx)
+	print('best rank',rankDict[bestRank])
 	if len(bestHandIdx) == 1:
-		print('best hand: ',possHands[bestHandIdx[0]],'\n')
-		return bestRank,possHands[bestHandIdx]
+		return possHands[bestHandIdx]
 	else:
-		tieWinnerIdx = determineTie(bestRank,possHands[bestHandIdx])
+		print("CHECKING TIES")
+		tieWinnerIdx = determineSelfTie(bestRank,possHands[bestHandIdx])
+		print("tie winner idx",tieWinnerIdx)
 		bestHand = possHands[bestHandIdx][tieWinnerIdx]
-	print('best hand: ',bestHand,'\n')
-	return bestRank, possHands[bestHandIdx]
+	print(bestHand,'\n')
+	pass
+	#return possHands[bestHandIdx]
 
 
-def determineTie(rank,tiedHands):
+def determineSelfTie(rank,tiedHands):
 	allNums = []
 	allCounts = []
 	for i in range(len(tiedHands)):
@@ -68,7 +155,6 @@ def determineTie(rank,tiedHands):
 		allCounts.append(count)
 		allNums.append(nums)
 	bestHandIdx = 0
-
 	if rank == HIGH_CARD:
 		#need to compare all cards
 		for i in range(1,len(tiedHands)):
@@ -79,69 +165,59 @@ def determineTie(rank,tiedHands):
 				elif allNums[i][j] < allNums[bestHandIdx][j]:
 					break
 		return bestHandIdx
-
 	elif rank == PAIR:
 		#need to compare 3 kickers, pair value is shared for single player possible hands
-		bestPairVal = allNums[bestHandIdx][np.argmax(allCounts[bestHandIdx])]
-		bestKickers = [num for num in allNums[bestHandIdx] if num!=bestPairVal]
+		pairVal = allNums[bestHandIdx][np.argmax(allCounts[bestHandIdx])]
+		print('pair value:', pairVal)
+		bestKickers = [num for num in allNums[bestHandIdx] if num!=pairVal]
+		print('checking against kickers', bestKickers)
 		for i in range(1,len(tiedHands)):
-			pairVal = allNums[i][np.argmax(allCounts[i])]
 			kickers = [num for num in allNums[i] if num!=pairVal]
-			if pairVal > bestPairVal:
-				bestHandIdx = i
-				bestPairVal = pairVal
-				bestKickers = kickers
-				continue
-			elif pairVal < bestPairVal:
-				continue
+			print('new kickers',kickers)
 			for j in range(2,-1,-1):
 				if kickers[j] > bestKickers[j]:
 					bestHandIdx = i
-					bestKickers = kickers
 					break
 				elif kickers[j] < bestKickers[j]:
 					break
 		return bestHandIdx
-
 	elif rank == TWO_PAIR:
-		#need to compare the 2 pairs and then last kicker
 		bestTwoPairIdx = np.where(allCounts[bestHandIdx]==2)[0]
 		bestTwoPairVals = [allNums[bestHandIdx][bestTwoPairIdx[0]],allNums[bestHandIdx][bestTwoPairIdx[1]+2]]
 		bestKicker = [num for num in allNums[bestHandIdx] if num not in bestTwoPairVals][0]
 		for i in range(1,len(tiedHands)):
-			twoPairIdx  = np.where(allCounts[i]==2)[0]
+			twoPairIdx = bestTwoPairIdx = np.where(allCounts[i]==2)[0]
 			twoPairVals = [allNums[i][twoPairIdx[0]],allNums[i][twoPairIdx[1]+2]]
 			kicker = [num for num in allNums[i] if num not in twoPairVals][0]
 			if twoPairVals < bestTwoPairVals:
 				continue
 			if twoPairVals > bestTwoPairVals or kicker > bestKicker:
 				bestHandIdx = i
-				bestTwoPairVals = twoPairVals
-				bestKicker = kicker
 		return bestHandIdx
 
+		#need to compare the 2 pairs and then last kicker
+		pass
 	elif rank == THREE_OF_A_KIND:
 		#need to compare 2 kickers, trips will be shared for single player possible hands
 		tripVal = allNums[bestHandIdx][np.argmax(allCounts[bestHandIdx])]
 		bestKickers = [num for num in allNums[bestHandIdx] if num != tripVal]
+		print('checking against kickers', bestKickers)
 		for i in range(1,len(tiedHands)):
 			kickers = [num for num in allNums[i] if num != tripVal]
+			print('new kickers',kickers)
 			for j in range(1,-1,-1):
 				if kickers[j] > bestKickers[j]:
 					bestHandIdx=i
-					beskKickers = kickers
 					break
 				elif kickers[j] < bestKickers[j]:
 					break
 		return bestHandIdx
-
 	elif rank == STRAIGHT:
 		#only need to compare top card
 		for i in range(1,len(tiedHands)):
 			if allNums[i][4] > allNums[bestHandIdx][4]:
 				bestHandIdx = i
 		return bestHandIdx
-
 	elif rank == FLUSH:
 		#need to compare all cards
 		for i in range(1,len(tiedHands)):
@@ -152,47 +228,15 @@ def determineTie(rank,tiedHands):
 				elif allNums[i][j] < allNums[bestHandIdx][j]:
 					break
 		return bestHandIdx
-
 	elif rank == FULL_HOUSE:
 		#need to compare trips then pair
-		bestTripVal = allNums[bestHandIdx][np.argmax(allCounts[bestHandIdx])+2]
-		bestPairVal = [num for num in allNums[bestHandIdx] if num != bestTripVal][0]
-		for i in range(1,len(tiedHands)):
-			tripVal = allNums[i][np.argmax(allCounts[i])+2]
-			pairVal = [num for num in allNums[i] if num !=bestTripVal][0]
-			if tripVal < bestTripVal:
-				continue 
-			elif tripVal > bestTripVal:
-				bestHandIdx = i
-				bestTripVal = tripVal
-				bestPairVal = pairVal
-				continue
-			elif pairVal > bestPairVal:
-				bestHandIdx = i
-		return bestHandIdx
-
-	elif rank == FOUR_OF_A_KIND:
-		#need to compare single kicker
-		bestQuadIdx = np.argmax(allCounts[bestHandIdx])
-		if bestQuadIdx == 0:
-			bestKicker = allNums[bestHandIdx][4]
-		else:
-			bestKicker = allNums[bestHandIdx][0]
-		for i in range(1,len(tiedHands)):
-			quadIdx = np.argmax(allCounts[i])
-			if bestQuadIdx == 0:
-				kicker = allNums[i][4]
-			else:
-				kicker = allNums[i][0]
-			if kicker < bestKicker:
-				continue
-			if kicker > bestKicker:
-				bestHandIdx = i
-				bestKicker = kicker
-		return bestHandIdx
-
+		'''
+		bestTripVal =
+		bestPairVal =
+		'''
+		pass
 	elif rank == STRAIGHT_FLUSH:
-		#need to compare top card
+		#Only need to compare top card
 		for i in range(1,len(tiedHands)):
 			if allNums[i][4] > allNums[bestHandIdx][4]:
 				bestHandIdx = i
@@ -214,8 +258,6 @@ def determineHand(hand):
 		return FULL_HOUSE
 	elif checkFlush(suits):
 		return FLUSH
-	elif checkStraight(nums):
-		return STRAIGHT
 	elif checkThreeOfKind(counts):
 		return THREE_OF_A_KIND
 	elif checkTwoPair(counts):
@@ -235,9 +277,10 @@ def checkStraight(nums):
 	if nums == [2,3,4,5,14]:
 		return True
 	for i in range(4):
-		if nums[i] != nums[i+1] - 1:
+		if nums[i] != nums[i+1] + 1:
 			return False
-	return True
+		else:
+			return True
 
 def checkRoyalFlush(suit,nums):
 	topCard = nums[4]
